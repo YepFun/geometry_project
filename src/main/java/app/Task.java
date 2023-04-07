69d13d6ec36b521dcfab26828138ead5b3fc8f9c
Rexi239 <113353063+Rexi239@users.noreply.github.com>
2023-04-07 20:39:37 +0300

    solve demo

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -1,6 +1,7 @@
 package app;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.humbleui.jwm.MouseButton;
@@ -16,6 +17,9 @@ import panels.PanelLog;
 
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static app.Colors.CROSSED_COLOR;
+import static app.Colors.SUBTRACTED_COLOR;
 
 /**
  * Класс задачи
@@ -53,6 +57,18 @@ public class Task {
      * Флаг, решена ли задача
      */
     private boolean solved;
+    /**
+     * Список точек в пересечении
+     */
+    @Getter
+    @JsonIgnore
+    private final ArrayList<Point> crossed;
+    /**
+     * Список точек в разности
+     */
+    @Getter
+    @JsonIgnore
+    private final ArrayList<Point> single;
 
     /**
      * Задача
@@ -67,6 +83,9 @@ public class Task {
     ) {
         this.ownCS = ownCS;
         this.points = points;
+
+        this.crossed = new ArrayList<>();
+        this.single = new ArrayList<>();
     }
 
     /**
@@ -76,12 +95,21 @@ public class Task {
      * @param windowCS СК окна
      */
     public void paint(Canvas canvas, CoordinateSystem2i windowCS) {
+        // Сохраняем последнюю СК
+        lastWindowCS = windowCS;
+
         canvas.save();
         // создаём перо
         try (var paint = new Paint()) {
             for (Point p : points) {
-                // получаем цвет точки
-                paint.setColor(p.getColor());
+                if (!solved) {
+                    paint.setColor(p.getColor());
+                } else {
+                    if (crossed.contains(p))
+                        paint.setColor(CROSSED_COLOR);
+                    else
+                        paint.setColor(SUBTRACTED_COLOR);
+                }
                 // y-координату разворачиваем, потому что у СК окна ось y направлена вниз,
                 // а в классическом представлении - вверх
                 Vector2i windowPos = windowCS.getCoords(p.pos.x, p.pos.y, ownCS);
@@ -90,10 +118,7 @@ public class Task {
             }
         }
         canvas.restore();
-        // Сохраняем последнюю СК
-        lastWindowCS = windowCS;
     }
-
     /**
      * Добавить точку
      *
@@ -168,9 +193,35 @@ public class Task {
      * Решить задачу
      */
     public void solve() {
+        // очищаем списки
+        crossed.clear();
+        single.clear();
+
+        // перебираем пары точек
+        for (int i = 0; i < points.size(); i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                // сохраняем точки
+                Point a = points.get(i);
+                Point b = points.get(j);
+                // если точки совпадают по положению
+                if (a.pos.equals(b.pos) && !a.pointSet.equals(b.pointSet)) {
+                    if (!crossed.contains(a)){
+                        crossed.add(a);
+                        crossed.add(b);
+                    }
+                }
+            }
+        }
+
+        /// добавляем вс
+        for (Point point : points)
+            if (!crossed.contains(point))
+                single.add(point);
+
+        // задача решена
         solved = true;
-        PanelLog.warning("Вызван метод solve()\n Пока что решения нет");
     }
+
     /**
      * Отмена решения задачи
      */
