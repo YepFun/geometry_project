7330def0b1796756aecc7d17de3899b2ea3d6971
Rexi239 <113353063+Rexi239@users.noreply.github.com>
2023-04-08 18:27:05 +0300

    scale works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -330,4 +330,19 @@ public class Task {
         return ownCS.getCoords(x, y, windowCS);
     }
 
+    /**
+     * Масштабирование области просмотра задачи
+     *
+     * @param delta  прокрутка колеса
+     * @param center центр масштабирования
+     */
+    public void scale(float delta, Vector2i center) {
+        if (lastWindowCS == null) return;
+        // получаем координаты центра масштабирования в СК задачи
+        Vector2d realCenter = ownCS.getCoords(center, lastWindowCS);
+        // выполняем масштабирование
+        ownCS.scale(1 + delta * WHEEL_SENSITIVE, realCenter);
+    }
+
+
 }
